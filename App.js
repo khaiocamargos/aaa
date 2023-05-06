2f9e4258928bdb401eb4e1a7bcf879449b8c883d
Khaio Camargos <khaioloco@gmail.com>
2023-05-06 14:47:28 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+
+export default function App() {
+  useEffect(() => {
+    registerForPushNotificationsAsync();
+  }, []);
+
+  async function registerForPushNotificationsAsync() {
+    let token;
+    if (Constants.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+  }
+
+  async function sendNotification() {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'notifiteste!',
+        body: 'Sua notificação',
+        sound: 'default',
+        "icon": "./assets/notification.png", // Substitua com a URL do seu ícone
+      },
+      trigger: null,
+    });
+  }
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={sendNotification} style={styles.button}>
+        <Text style={styles.buttonText}>Enviar notificação</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#fff',
+    padding: 20,
+  },
+  button: {
+    backgroundColor: '#007AFF',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
